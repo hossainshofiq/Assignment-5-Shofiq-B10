640f2af7048eb7b427dfee3b3e4bc119f8868374
hossainshofiq <ah731710@gmail.com>
2024-09-25 03:57:52 +0600

    update some codes

--- js/donation.js
+++ js/donation.js
@@ -8,3 +8,26 @@ document.getElementById('toggle-history-btn').addEventListener ('click', functio
     toggleButton ('donation-history');
     toggleBtnColor (event.target.id);
 })
+
+document.getElementById('donation-btn-noakhali').addEventListener ('click', function(){
+    const donatedAmount = getValueFromInputField('input-donation-noakhali');
+    let userWallet = getValueFromInnerText ('user-wallet');
+    const areaTitle = getStringFromInnerText('noakhali');
+    donationBtnAction ('noakhali-initial-balance', userWallet, donatedAmount, areaTitle);    
+   })
+
+
+document.getElementById('donation-btn-feni').addEventListener ('click', function(){
+    const donatedAmount = getValueFromInputField('input-donation-feni');
+    let userWallet = getValueFromInnerText ('user-wallet');
+    const areaTitle = getStringFromInnerText('feni');
+    donationBtnAction ('feni-initial-balance', userWallet, donatedAmount, areaTitle);     
+})
+
+
+document.getElementById('donation-btn-quota').addEventListener ('click', function(){
+    const donatedAmount = getValueFromInputField('input-donation-quota');
+    let userWallet = getValueFromInnerText ('user-wallet');
+    const areaTitle = getStringFromInnerText('quota-protest');
+    donationBtnAction ('quota-initial-balance', userWallet, donatedAmount, areaTitle);     
+})
